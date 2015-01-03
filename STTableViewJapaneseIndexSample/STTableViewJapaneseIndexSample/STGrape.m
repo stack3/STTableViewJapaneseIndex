42f62f086e6813ed070ee0de3eee24282a346f78
EIMEI <miyamoto@stack3.net>
2015-01-03 13:27:17 +0900

    Author変更

--- STTableViewJapaneseIndexSample/STTableViewJapaneseIndexSample/STGrape.m
+++ STTableViewJapaneseIndexSample/STTableViewJapaneseIndexSample/STGrape.m
@@ -2,7 +2,7 @@
 //  STGrape.m
 //  STTableViewJapaneseIndexSample
 //
-//  Created by MIYAMOTO, Hideaki on 2015/01/02.
+//  Created by EIMEI on 2015/01/02.
 //  Copyright (c) 2015年 stack3. All rights reserved.
 //
 
